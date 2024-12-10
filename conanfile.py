a38481a0f228b6c524cedc53286fd220d1a2affd
Wael Karman <wael.karman@ar-lx0275.agile-robots.de>
2024-12-10 22:37:56 +0100

    clean

--- conanfile.py
+++ conanfile.py
@@ -1,37 +1,23 @@
-from conanfile import ConanFile, CMake
+from conans import ConanFile, CMake
 
-
-class HelloConan(ConanFile):
-    name = "Hello"
-    version = "0.1"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Hello here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+class HelloWorldConan(ConanFile):
+    name = "HelloWorld"
+    version = "1.0"
+    license = "MIT"
+    author = "Your Name <your.email@example.com>"
+    description = "Simple Hello World C++ project"
+    topics = ("conan", "hello world", "cpp")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    generators = "cmake"
-    exports_sources = "src/*"
+    requires = []  # Nessuna dipendenza esterna
+    generators = "cmake"  # Usa CMake come generatore
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="src")
+        cmake.configure()
         cmake.build()
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
-        self.copy("*.h", dst="include", src="src")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.dylib*", dst="lib", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*", dst="bin", src="bin")
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.bindirs = ["bin"]
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Conan!" << std::endl;
+    return 0;
+}
